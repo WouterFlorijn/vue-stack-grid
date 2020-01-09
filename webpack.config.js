8c6cb3b0f867540c45588251a42d34224ba34756
Wouter Florijn <woutermflorijn@gmail.com>
2020-01-09 14:22:16 +0100

    Update/add packages.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+};
